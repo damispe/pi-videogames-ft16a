cacee370fb25ac15ca1d8192e8ac7524cf1ca0cd
damispe <damianspescha@gmail.com>
2021-08-30 14:53:00 -0300

    mount db

--- api/src/models/Genre.js
+++ api/src/models/Genre.js
@@ -3,26 +3,12 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Videogame', {
+  sequelize.define('Genre', {
     id: {
       type: DataTypes.UUID,
       primaryKey: true,
     },
     name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    release_date:{
-      type: DataTypes.STRING,
-    },
-    rating: {
-      type: DataTypes.STRING
-    },
-    platforms: {
       type: DataTypes.STRING,
       allowNull: false,
     },
